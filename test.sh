0ec337730cb0502116582c7ecc52e0afbe103c66
preetha <preethushetty6722@gmail.com>
2025-07-29 23:53:42 +0000

    new

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+collegename="pesiams"
+degree="BCA"
+
+echo "one of my friend studinin in $collegename in $degree"
+
+
+echo -e " how are you\t then how is life\n whats going on \v"
+
+echo "${SHELL}"
+
+echo "${PWD}"
+
+echo "${HOME}"
+
+
+
+
+echo "my name is ${1} and age is ${2}"
